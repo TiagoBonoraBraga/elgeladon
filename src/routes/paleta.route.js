68b09da0b295f1e7fc3b5e0ae0838331e15a3c87
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-07-05 15:26:19 -0300

    chore: swagger

--- src/routes/paleta.route.js
+++ src/routes/paleta.route.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
 const controllerPaletas = require('../controllers/paleta.controller');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
+
 const {
   validId,
   validObjectBody,
 } = require('../middlewares/paleta.middleware');
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.get('/all-paletas', controllerPaletas.findAllPaletasController);
 router.get(
